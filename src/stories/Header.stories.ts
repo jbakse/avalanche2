bf60322581f95069aa916bd1f6fe2239ba138644
Justin Bakse <justin@tsfim.com>
2024-09-18 11:37:00 -0500

    clean header

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,38 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
-
 import { Header } from "./Header";
 
 const meta = {
   title: "Avalanche/Header",
   component: Header,
-  tags: ["autodocs"],
-  parameters: {
-    layout: "fullscreen",
-  },
-  args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
-  },
+  parameters: { layout: "fullscreen" },
 } satisfies Meta<typeof Header>;
-
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const LoggedIn: Story = {
-  args: {
-    title: "Avalanche",
-    user: {
-      name: "Jane Doe",
-      avatarUrl: "https://dummyimage.com/128",
-      isAdmin: false,
-    },
-  },
-};
-
-export const LoggedOut: Story = {
+export const Default = {} satisfies Story;
+export const Titled = {
   args: {
     title: "Avalanche",
   },
-};
+} satisfies Story;
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,60 +1,10 @@
-// import React from "react";
-
-import { Button } from "./Button";
-
-type User = {
-  name: string;
-  avatarUrl: string;
-  isAdmin: boolean;
-};
-
 interface HeaderProps {
-  user?: User;
-  title: string;
-  onLogin?: () => void;
-  onLogout?: () => void;
-  onPostSketch?: () => void;
-  onCreateAccount?: () => void;
+  title?: string;
 }
 
-export const Header = ({
-  user,
-  title = "Untitled",
-  onLogin,
-  onLogout,
-  onPostSketch,
-  onCreateAccount,
-}: HeaderProps) => (
+export const Header = ({ title = "Untitled" }: HeaderProps) => (
   <header className="flex items-center font-sans border-b h-28">
     <h1 className="px-8 text-2xl uppercase">{title}</h1>
-    <div className="ml-auto px-8 flex items-center">
-      {user ? (
-        <>
-          <Button onClick={onPostSketch} label="Post a Sketch" />
-          <img
-            className={classes(
-              "ml-4 rounded-full w-16 h-16",
-              user.isAdmin ? "border-4 border-red-500" : ""
-            )}
-            src={user.avatarUrl}
-          />
-        </>
-      ) : (
-        <>
-          <Button onClick={onLogin} label="Log in" />
-          <Button
-            // className="ml-4"
-            primary
-            className="ml-4"
-            onClick={onCreateAccount}
-            label="Sign up"
-          />
-        </>
-      )}
-    </div>
+    <div className="ml-auto px-8 flex items-center"></div>
   </header>
 );
-
-function classes(...args: string[]) {
-  return args.join(" ");
-}
